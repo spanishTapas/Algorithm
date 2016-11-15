2e4ec7bfec1fc3cb2a8638d35902b91c8568700d
wanming zhang <wzhang2@apple.com>
2016-11-14 18:38:20 -0800

    SameTree code cleanup.

--- src/tree/SameTree.java
+++ src/tree/SameTree.java
@@ -1,5 +1,22 @@
 package tree;
+/**
+ * 
+ * Given two binary trees, write a function to check if they are equal or not.
 
+Two binary trees are considered equal if they are structurally identical and the nodes have the same value.
+
+
+ *
+ */
 public class SameTree {
-
+	
+	public boolean isSameTree(TreeNode p, TreeNode q) {
+		if (p == null && q == null) { // both empty
+			return true;
+		} else if (p != null && q != null) { // both non-empty
+			return (p.val == q.val) && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+		} else { // only one empty
+			return false;
+		}
+	}
 }
