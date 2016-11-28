f0047feda44389377317c3e92ccec686cfe00660
wanming zhang <wzhang2@apple.com>
2016-11-28 15:02:16 -0800

    Path Sum II.

--- src/tree/PathSum.java
+++ src/tree/PathSum.java
@@ -1,12 +1,82 @@
 package tree;
 
+import java.util.*;
+
 public class PathSum {
-
+/**
+              5
+             / \
+            4   8
+           /   / \
+          11  13  4
+         /  \    / \
+        7    2  5   1
+**/
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Construct tree
+		TreeNode node7 = newNode(7);
+		TreeNode node2 = newNode(2);
+		TreeNode node11 = newNode(11);
+		
+		TreeNode node4a = newNode(4);
+		TreeNode node5 = newNode(5);
+		
+		TreeNode node1 = newNode(1);
+		TreeNode node5a = newNode(5);
+		TreeNode node4b = newNode(4);
+		TreeNode node13 = newNode(13);
+		TreeNode node8 = newNode(8);
+		
+		node5.left = node4a;
+		node5.right = node8;
+		
+		node4a.left = node11;
+		node8.left = node13;
+		node8.right = node4b;
+		
+		node11.left = node7;
+		node11.right = node2;
+		node4b.left = node5a;
+		node4b.right = node1;
+		
+		 printTree(node5);
+		 System.out.println();
+		 
+		 List<List<Integer>> result = pathSumII(node5, 22); 
+		 System.out.println("path sum II: " + result.size() + " paths.");
+		 for (List<Integer> path : result) {
+			 System.out.print("[");
+			 for (int num : path) {
+				 
+				 System.out.print(num + ",");
+			 }
+			 System.out.print("]");
+		 }
+		 
 	}
+	
+	// print inorder
+	private static void printTree(TreeNode node) {
+	 if (node == null) return;
+	 // left, node itself, right
+	 printTree(node.left);
+	 System.out.print(node.val + "  ");
+	 printTree(node.right);
+	}
+	
 
+	/*
+	 Helper function that allocates a new node
+	 with the given data and NULL left and right
+	 pointers.
+	 */
+	public static TreeNode newNode(int data) {
+	  TreeNode node = new TreeNode(data);
+	  node.left = null;
+	  node.right = null;
+	  return node;
+	}
+	
 	/**
 	 * PathSum
 	 * Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
@@ -91,5 +161,52 @@ public class PathSum {
 		return rootPath + helper(node.left, currSum, sum) + helper(node.right, currSum, sum); 
 	}
 	
+	/**
+	 * Given a binary tree and a sum, find all root-to-leaf paths where each path's sum equals the given sum.
+
+	 * For example:
+     * Given the below binary tree and sum = 22,
+     *        5
+             / \
+            4   8
+           /   / \
+          11  13  4
+         /  \      \
+        7    2      1
+       
+    return 
+    [
+   		[5,4,11,2],
+   		[5,8,4,5]
+	]
+	 */
+	
+    public static List<List<Integer>> pathSumII(TreeNode root, int sum) {
+    	List<List<Integer>> result = new ArrayList<List<Integer>>();
+    	List<Integer> path = new ArrayList<Integer>();
+    	pathSumHelper(result, path, root, sum);
+    	return result;
+    }
+	
+    public static void pathSumHelper(List<List<Integer>> result, List<Integer> path, TreeNode root, int sum){
+       
+    	if (root == null) {
+    		return;
+    	}
+    	sum = sum - root.val;
+    	
+    	if (root.left == null && root.right == null) {
+    		if (sum == 0) {
+    			path.add(root.val);
+    			result.add(new ArrayList<Integer>(path));
+    			path.remove(path.size()-1);
+    		}
+    	}
+    	
+    	path.add(root.val);
+    	pathSumHelper(result, path, root.left, sum);
+    	pathSumHelper(result, path, root.right, sum);
+    	path.remove(path.size()-1);
+    }
 	
 }
