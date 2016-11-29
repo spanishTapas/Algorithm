0aa0c331e71630d2e6b76e98b048183cbfeebe4b
wanming zhang <wzhang2@apple.com>
2016-11-28 16:32:27 -0800

    Delete duplicate node from sorted list.

--- src/linked_list/DeleteNode.java
+++ src/linked_list/DeleteNode.java
@@ -10,12 +10,55 @@ package linked_list;
 public class DeleteNode {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ListNode head = new ListNode(1);
+		ListNode node1 = new ListNode(1);
+		ListNode node2a = new ListNode(2);
+		ListNode node2b = new ListNode(2);
+		ListNode node3a = new ListNode(3);
+		ListNode node3b = new ListNode(3);
+		
+		head.next = node1;
+		node1.next = node2a;
+		node2a.next = node2b;
+		node2b.next = node3a;
+		node3a.next = node3b;
+		
+		ListNode result = deleteDuplicateNode(head);
+		
+		while (result != null) {
+			System.out.print(result.val + ",");
+			result = result.next;
+		}
 
 	}
 	public void deleteNode(ListNode node) {
 		node.val = node.next.val;
 		node.next = node.next.next;
 	}
+	
+	/**
+	 * Given a sorted linked list, delete all duplicates such that each element appear only once.
 
+	For example,
+	Given 1->1->2, return 1->2.
+	Given 1->1->2->3->3, return 1->2->3.
+	 **/
+
+	public static ListNode deleteDuplicateNode(ListNode head) {
+		if (head == null) {
+			return head;
+		}
+		
+		ListNode tmp = head;
+		
+		while (tmp.next != null) {
+			if (tmp.val == tmp.next.val) {
+				tmp.next = tmp.next.next;
+			} else {
+				tmp = tmp.next;
+			}
+		}
+		return head;
+	}
+	
 }
